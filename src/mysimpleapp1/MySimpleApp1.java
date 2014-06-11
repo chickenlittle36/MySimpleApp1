01ba7948fae2b910db990f259fb321b794863eb9
panida <on_sky36@hotmail.co.th>
2014-06-11 10:21:02 +0700

    MySimpleApp1

---
+++ src/mysimpleapp1/MySimpleApp1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mysimpleapp1;
+
+/**
+ *
+ * @author Administrator
+ */
+public class MySimpleApp1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+        
+    }
+
+}
